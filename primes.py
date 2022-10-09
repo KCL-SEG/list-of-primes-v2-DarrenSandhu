bafb894a682a67a87c428fecd4adedb792e84019
DarrenSandhu <115267021+DarrenSandhu@users.noreply.github.com>
2022-10-09 17:35:11 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
     list = []
+    n = 2
+
+    if (number_of_primes <= 0):
+        raise ValueError(f"x = {number_of_primes} should have been a number greater than 0.")
+        
+    while (len(list) != number_of_primes):
+        for i in range(2, n // 2 + 1):
+            if n % i == 0:
+                break
+        else:
+            list.append(n)
+        n += 1
+
     return list
